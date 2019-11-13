4adbc751a5bfa2776cf029b861809b9bdcca146a
davestroud <david@davidstroud.me>
2019-11-12 19:11:35 -0600

    reshape data

--- autoencoders.py
+++ autoencoders.py
@@ -6,4 +6,14 @@ from keras.datasets import mnist
 from keras.utils import plot_model
 from keras import backend as K
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load MNIST dataset
+(x_train, _), x_test, _) = mnist.load_data()
+
+# reshape to (28, 28, 1) and normalize input images
+image_size = x_train.shape[1]
+x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
+x_test = np.reshape(x_test, [1, image_size, image_size, 1])
 
