756566f1a606941972ff7b8897ecc8314189c2f3
davestroud <david@davidstroud.me>
2019-11-12 18:56:55 -0600

    add autoencoder file

---
+++ autoencoders.py
@@ -0,0 +1,9 @@
+from keras.layers import Dense, Input
+from keras.layers import Conv2D, Flatten
+from keras.layers import Reshape, Conv2DTranspose
+from keras.models import Model
+from keras.datasets import mnist
+from keras.utils import plot_model
+from keras import backend as K
+
+
