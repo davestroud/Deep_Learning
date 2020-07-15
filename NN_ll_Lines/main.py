17b50a73873d1e65f498dad8fb5fcc512fed8c32
dstroud <david@davidstroud.me>
2020-07-15 11:19:55 -0500

    add dataset

--- NN_ll_Lines/main.py
+++ NN_ll_Lines/main.py
@@ -1,12 +1,19 @@
-X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-y = np.array([[0, 1, 1, 0]]).T
-syn0 = 2*np.random.random((3, 4)) - 1
-syn1 = 2*np.random.random((4, 1)) - 1
-for j in xrange(60000):
-    l1 = 1/(1+np.exp(-(np.dot(X, syn0))))
-    l2 = 1/(1+np.exp(-(np.dot(l1, syn1))))
-    l2_delta = (y - l2)*(l2*(1-l2))
-    l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += X.T.dot(l1_delta)
-asdfasdfasdfasdfasdfasdfasdfasdfasdf
+import numpy as np
+
+# sigmoid function
+
+
+def nonlin(x, deriv=False):
+    if(deriv == True):
+        return x * (1-x)
+    return 1/(1+np.exp(-x))
+
+
+# input dataset
+X = np.array([[0, 0, 1]
+                [0, 1, 1],
+                [1, 0, 1],
+                [1, 1, 1]]])
+
+# output datatset
+y = np.array([[0, 0, 1, 1]]).T
