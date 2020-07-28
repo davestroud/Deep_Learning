ef4163809460e69a83f317655b022e5a98d283e2
dstroud <david@davidstroud.me>
2020-07-27 20:41:04 -0500

    write out first few lines of nn code

--- Pytorch/Classification/.ipynb_checkpoints/classification_utils-checkpoint.py
+++ Pytorch/Classification/.ipynb_checkpoints/classification_utils-checkpoint.py
@@ -4,3 +4,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+class Model(nn.Module):
+    
+    def __init__(self, embedding_size, num_numerical_cols, output_size, layers, p=0.4):
+        super().init __init__()
+        self.all_embeddings = nn.ModuleList([nn.Embedding(ni, nf) for ni, nf in embedding_size])
