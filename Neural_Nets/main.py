613f725f38f180088d161b4dd06a9110430b9694
dstroud <david@davidstroud.me>
2021-03-31 14:58:49 -0500

    add network object

--- Neural_Nets/main.py
+++ Neural_Nets/main.py
@@ -1,2 +1,15 @@
 
 from keras.datasets import mnist
+import numpy as np
+
+
+# Initialize a network object
+class Network(object):
+    
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+        
